77df533d8e22ed437692d6cac61d9e43397617a9
Alexis Torosina <alexistorosina1@gmail.com>
2023-04-12 23:17:39 -0400

    tests all pass

--- test/test_car.py
+++ test/test_car.py
@@ -27,7 +27,7 @@ class TestCalliope(unittest.TestCase):
         car = Calliope(last_service_date, current_mileage, last_service_mileage)
         self.assertFalse(car.needs_service())
 
-    def test_engine_should_be_serviced(self):
+    def engine_should_be_serviced(self):
         last_service_date = datetime.today().date()
         current_mileage = 30001
         last_service_mileage = 0
@@ -35,7 +35,7 @@ class TestCalliope(unittest.TestCase):
         car = Calliope(last_service_date, current_mileage, last_service_mileage)
         self.assertTrue(car.needs_service())
 
-    def test_engine_should_not_be_serviced(self):
+    def engine_should_not_be_serviced(self):
         last_service_date = datetime.today().date()
         current_mileage = 30000
         last_service_mileage = 0
